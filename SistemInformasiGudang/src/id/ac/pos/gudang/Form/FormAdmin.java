745e6985daf2eb4d76e234d6e78f8dfafe7f75cd
reyhanadp <reyhanadp@yahoo.co.id>
2017-12-09 13:34:43 +0700

    form admin udah cuman belum link ke login

--- SistemInformasiGudang/src/id/ac/pos/gudang/Form/FormAdmin.java
+++ SistemInformasiGudang/src/id/ac/pos/gudang/Form/FormAdmin.java
@@ -5,6 +5,9 @@
  */
 package id.ac.pos.gudang.Form;
 
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
 /**
  *
  * @author reyha
@@ -27,10 +30,13 @@ public class FormAdmin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        buttonKelolaProduk = new javax.swing.JButton();
-        buttonKelolaProduk1 = new javax.swing.JButton();
-        buttonKelolaProduk2 = new javax.swing.JButton();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         itemRegional = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -41,40 +47,75 @@ public class FormAdmin extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/logo-kecil.png"))); // NOI18N
+        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
-        buttonKelolaProduk.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons/kelola_produk1.png"))); // NOI18N
-        buttonKelolaProduk.setText("KELOLA PRODUK");
-        buttonKelolaProduk.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        buttonKelolaProduk.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
-        buttonKelolaProduk.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        buttonKelolaProduk.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                buttonKelolaProdukActionPerformed(evt);
-            }
-        });
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
+        jLabel1.setText("ADMIN DASHBOARD");
 
-        buttonKelolaProduk1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons/kelola_produk1.png"))); // NOI18N
-        buttonKelolaProduk1.setText("KELOLA PRODUK");
-        buttonKelolaProduk1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        buttonKelolaProduk1.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
-        buttonKelolaProduk1.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        buttonKelolaProduk1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                buttonKelolaProduk1ActionPerformed(evt);
-            }
-        });
+        jSeparator1.setForeground(new java.awt.Color(0, 0, 0));
+        jSeparator1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
 
-        buttonKelolaProduk2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons/kelola_produk1.png"))); // NOI18N
-        buttonKelolaProduk2.setText("KELOLA PRODUK");
-        buttonKelolaProduk2.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        buttonKelolaProduk2.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
-        buttonKelolaProduk2.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-        buttonKelolaProduk2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                buttonKelolaProduk2ActionPerformed(evt);
-            }
-        });
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons/icon-mitra-1.png"))); // NOI18N
+        jLabel2.setText("PARTNERS");
+        jLabel2.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jLabel2.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons/icon-user-1.png"))); // NOI18N
+        jLabel3.setText("USER");
+        jLabel3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jLabel3.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons/icon-regional-1.png"))); // NOI18N
+        jLabel4.setText("REGIONAL");
+        jLabel4.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jLabel4.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons/icon-logout-1.png"))); // NOI18N
+        jButton1.setText("Logout");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel3)
+                .addGap(92, 92, 92)
+                .addComponent(jLabel2)
+                .addGap(97, 97, 97)
+                .addComponent(jLabel4)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jSeparator1)
+                    .addComponent(jLabel1))
+                .addGap(183, 183, 183)
+                .addComponent(jButton1)
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jButton1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4))
+                .addGap(50, 50, 50))
+        );
 
         itemRegional.setText("File");
 
@@ -122,24 +163,16 @@ public class FormAdmin extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(buttonKelolaProduk, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(18, 18, 18)
-                .addComponent(buttonKelolaProduk1, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(buttonKelolaProduk2, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(buttonKelolaProduk2, javax.swing.GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(buttonKelolaProduk1, javax.swing.GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE)
-                        .addComponent(buttonKelolaProduk, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -160,23 +193,11 @@ public class FormAdmin extends javax.swing.JFrame {
 
     }//GEN-LAST:event_itemHistoryDeleteActionPerformed
 
-    private void buttonKelolaProdukActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonKelolaProdukActionPerformed
-        // TODO add your handling code here:
-
-    }//GEN-LAST:event_buttonKelolaProdukActionPerformed
-
-    private void buttonKelolaProduk1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonKelolaProduk1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_buttonKelolaProduk1ActionPerformed
-
-    private void buttonKelolaProduk2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonKelolaProduk2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_buttonKelolaProduk2ActionPerformed
-
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+        
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -199,7 +220,13 @@ public class FormAdmin extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(FormAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        try {
+            com.jtattoo.plaf.noire.NoireLookAndFeel.setTheme("Large-Font", "Java Swing", "");
+            UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
+            new FormAdmin();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -209,16 +236,19 @@ public class FormAdmin extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton buttonKelolaProduk;
-    private javax.swing.JButton buttonKelolaProduk1;
-    private javax.swing.JButton buttonKelolaProduk2;
     private javax.swing.JMenuItem itemHistoryDelete;
     private javax.swing.JMenuItem itemHistoryUpdate;
     private javax.swing.JMenuItem itemRecycleBin;
     private javax.swing.JMenu itemRegional;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
     // End of variables declaration//GEN-END:variables
 }
